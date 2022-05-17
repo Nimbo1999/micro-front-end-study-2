ce94f0fea935ab6e0d7f5983fbbe19fdd454375e
Matheus Lopes <matlopes1999@gmail.com>
2022-05-17 08:10:30 -0300

    Fake commit

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/Marketing/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hello Word!</h1>
+      <h1>Hello Word! My Friend!</h1>
       <hr />
       <MarketingApp />
     </div>
