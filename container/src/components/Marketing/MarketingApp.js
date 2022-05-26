dc006596e2c1462d32480eb23d0a8b9f164a78e2
Matheus Lopes <=>
2022-05-26 14:33:19 -0300

    Add communication between 2 micro-frontends

--- container/src/components/Marketing/MarketingApp.js
+++ container/src/components/Marketing/MarketingApp.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 const MarketingApp = () => {
   const marketingRef = useRef(null);
+  const history = useHistory();
+
+  const onNavigate = ({ pathname: nextPathname }) => history.location.pathname !== nextPathname && history.push(nextPathname);
 
   useEffect(() => {
-    mount(marketingRef.current);
-  });
+    const { onParentNavigate } = mount(marketingRef.current, { onNavigate });
+    history.listen(onParentNavigate);
+  }, []);
 
   return (
     <div ref={marketingRef} />
